cc9d6f4b491465a401faf3debb3f593498f1085d
root <xinmin.he@yunzhanghu.com>
2019-07-02 18:08:54 +0800

    analyze more power

--- sql.go
+++ sql.go
@@ -48,6 +48,7 @@ func getPackagePaths(root string) ([]string, error) {
 type functionPara struct{
 	pName string
 	pType string
+	di * DbInput
 }
 
 type StateMentAnalysis int 
@@ -57,6 +58,99 @@ const (
 	StateMentAnalysis_FUNCTION
 	StateMentAnalysis_FUNCTION_BODY
 )
+
+type DbInput struct{
+	format string
+	paras [] *functionPara
+	
+}
+
+func (di *DbInput) filterEmptyPara(){
+	c := di.getParaCountFromFormat()
+	for i:= len(di.paras); i < c; i++{
+		di.paras = append(di.paras, nil)
+	}
+}
+
+func (di *DbInput) getParaCountFromFormat() int {
+	count := 0
+	var pre rune
+	for i, c := range di.format{
+		if i > 0 && c == '%' && pre != '\\' {
+			count = count + 1
+		}
+		pre = c
+	}
+	return count
+}
+
+func (di *DbInput) getFirstEmpty(start int) (int, bool){
+	for i , p := range di.paras{
+		if i >= start && p == nil{
+			return i, true
+		}
+	}
+	return 0, false
+}
+
+// index from 1
+func (di *DbInput) getFormatPos(index int) (int, rune, bool){
+	count := 1
+	var pre rune
+	for i, c := range di.format{
+		if i > 0 && c == '%' && pre != '\\' {
+			if count == index{
+				return i, pre, true
+			}
+			count = count + 1
+		}
+		pre = c
+	}
+	return 0, pre, false
+}
+
+func (di *DbInput) addFormat(input *DbInput) *DbInput{
+	di.format = input.format
+	di.paras = append(input.paras, di.paras...)
+	di.filterEmptyPara()
+	return di
+}
+
+func (di *DbInput) addParameter(index int, input *DbInput) *DbInput{
+	if input.format == ""{
+		// only parameters, find place
+		for _, p := range input.paras{
+			if i, ok := di.getFirstEmpty(0); ok{
+				di.paras[i] = p
+			}else{
+				panic(2)
+			}
+		}
+	} else{
+		// find format pos
+		if pos, preChar, ok := di.getFormatPos(index); ok{
+			if preChar == 's'{
+				// merge 
+				di.format = di.format[:pos - 1] + input.format
+				newParas := di.paras[:index - 1]
+				newParas = append(newParas, input.paras...)
+				newParas = append(newParas, di.paras[index - 1:]...)
+				di.paras = newParas
+			}else{
+				// set empty parameter
+				di.paras[index - 1] = &functionPara{}
+			}
+			
+		}else{
+			panic(3)
+		}
+	}
+	return di
+}
+
+func (di *DbInput) reportError(){
+}
+
 
 type Analyzer struct {
 	ignoreNosec bool
@@ -68,6 +162,8 @@ type Analyzer struct {
 	state       StateMentAnalysis
 	curFunName    string
 	logger      *log.Logger
+	dbCallPara   map[string]string
+	allPossibleInput map[string] *DbInput
 }
 
 func (si *Analyzer) isFunctionParaName() bool {
@@ -173,6 +269,7 @@ func (si *Analyzer) upDateStateAfterPop(){
 			if si.state == StateMentAnalysis_FUNCTION{
 				si.parameters = nil
 				si.curFunName = ""
+				si.allPossibleInput = make(map[string] *DbInput)
 				si.ChangeState(StateMentAnalysis_START)
 			}
 		}
@@ -217,6 +314,59 @@ func (si *Analyzer) isDbCallFunction() bool{
 	return false
 }
 
+func (si *Analyzer) getDbInputFromRhs(n ast.Node) *DbInput {
+	di := &DbInput{}
+	// fmt.printf
+	if callexp, ok := n.(*ast.CallExpr); ok {
+		if f, ok := callexp.Fun.(*ast.SelectorExpr); ok{
+			if x, ok := f.X.(*ast.Ident); ok {
+				if x.Name == "fmt" && f.Sel.Name == "Sprintf"{
+					// deal format
+					for i, arg := range callexp.Args{
+						if i == 0{
+							// ad format
+							addFormat := si.getDbInputFromRhs(arg)
+							di = di.addFormat(addFormat)
+						} else {
+							// add parameter
+							addParameter := si.getDbInputFromRhs(arg)
+							di = di.addParameter(i, addParameter)
+						}
+					}
+				}
+			}
+		}
+	}
+	// to do else
+	return di
+}
+
+func (si *Analyzer) AddDbCallPara(n string, t string){
+	switch t{
+	case "*sqlx.DB", "sqlx.Tx":
+		{
+			if _, ok := si.dbCallPara[n]; !ok{
+				si.dbCallPara[n] = t
+			}
+		}
+	}
+}
+
+func (si *Analyzer) isDbInterfaceCall(n ast.Node) (string, string, bool) {
+	if f, ok := n.(*ast.SelectorExpr); ok{
+		if x, ok := f.X.(*ast.Ident); ok{
+			if v, ok := si.dbCallPara[x.Name]; ok{
+				return v, f.Sel.Name, true
+			}
+		}
+	}
+	return "", "", false
+}
+
+func (si *Analyzer) checkDbCall(node *ast.CallExpr, iType string, fName string){
+	panic(1)
+}
+
 func (si *Analyzer) Visit(n ast.Node) ast.Visitor {
 	if n == nil{
 		si.upDateStateAfterPop()
@@ -226,52 +376,66 @@ func (si *Analyzer) Visit(n ast.Node) ast.Visitor {
 		si.caseStack.PushBack(n)
 		//fmt.Printf("push len is %d\n", si.caseStack.Len())
 		switch node:= n.(type){
-		case *ast.Field:
-			if si.isFunctionParaName(){
-				if len(node.Names) > 0{
-					//fmt.Printf("%s %s\n", node.Names[0].Name, getParaType(*node))
-					si.parameters = append(si.parameters, 
-						functionPara{pName:node.Names[0].Name, pType:getParaType(*node)})
+			case *ast.Field:
+				if si.isFunctionParaName(){
+					if len(node.Names) > 0{
+						//fmt.Printf("%s %s\n", node.Names[0].Name, getParaType(*node))
+						si.parameters = append(si.parameters, 
+							functionPara{pName:node.Names[0].Name, pType:getParaType(*node)})
+					}
+					si.AddDbCallPara(node.Names[0].Name, getParaType(*node))
+				}
+			case *ast.FuncDecl:
+				if	si.state == StateMentAnalysis_START{
+					si.curFunName = node.Name.Name
+					si.ChangeState(StateMentAnalysis_FUNCTION)
+				}
+			case *ast.BlockStmt:{
+				if	si.state == StateMentAnalysis_FUNCTION{
+					si.ChangeState(StateMentAnalysis_FUNCTION_BODY)
 				}
 			}
-		case *ast.FuncDecl:
-			if	si.state == StateMentAnalysis_START{
-				si.curFunName = node.Name.Name
-				si.ChangeState(StateMentAnalysis_FUNCTION)
-			}
-		case *ast.BlockStmt:{
-			if	si.state == StateMentAnalysis_FUNCTION{
-				si.ChangeState(StateMentAnalysis_FUNCTION_BODY)
-			}
-		}
-		case *ast.CallExpr:
-			//fmt.Printf("call expr %d\n", si.state )
-			if si.state == StateMentAnalysis_FUNCTION_BODY{
-				if si.isSprintfCall(n){
-					//fmt.Printf("is sprintf call\n")
-					if len(node.Args) >= 2{
-						switch format := node.Args[0].(type){
-						case *ast.BasicLit:
-							for i, para := range(node.Args){
-								if i > 0{
-									switch p := para.(type){
-									case *ast.Ident:
-										if isStringFormat(format.Value, i) && si.isFunctionParameters(p.Name) && si.isDbCallFunction(){
-											fmt.Printf("sql injectiton:%s\n", si.curFunName)
+			case *ast.CallExpr:{
+				//fmt.Printf("call expr %d\n", si.state )
+				if si.state == StateMentAnalysis_FUNCTION_BODY{
+					if si.isSprintfCall(n){
+						//fmt.Printf("is sprintf call\n")
+						if len(node.Args) >= 2{
+							switch format := node.Args[0].(type){
+							case *ast.BasicLit:
+								for i, para := range(node.Args){
+									if i > 0{
+										switch p := para.(type){
+										case *ast.Ident:
+											if isStringFormat(format.Value, i) && si.isFunctionParameters(p.Name) && si.isDbCallFunction(){
+												fmt.Printf("sql injectiton:%s\n", si.curFunName)
+											}
 										}
 									}
-								
 								}
 							}
 						}
 					}
 					
+					if iType, fName, ok := si.isDbInterfaceCall(n); ok{
+						si.checkDbCall(node, iType, fName)
+					}
+					
 				}
 			}
-
+			case *ast.AssignStmt:{
+				if si.state == StateMentAnalysis_FUNCTION_BODY{
+					// del right
+					dbInput := si.getDbInputFromRhs(node.Rhs[0])
+					
+					if dbInput.format != ""{
+						if v, ok := node.Lhs[0].(*ast.Ident); ok{
+							si.allPossibleInput[v.Name] = dbInput
+						}
+					}
+				}
+			}
 		}
-
-		
 		return si
 	}
 }
@@ -384,7 +548,7 @@ func main(){
 		caseStack:   list.New(),
 		parameters:       make([]functionPara,1),
 		state:       StateMentAnalysis_START,
-		
+		allPossibleInput: make(map[string]*DbInput),
 	}
 	si.CheckDir(*checkDir)
 	//fmt.Println(getPackagePaths(*checkDir))
